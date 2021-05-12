afc95fb5c0391682efb32bcae54cdb96565ae62d
Ali Kabalan <ali.kabalan.1994@gmail.com>
2021-05-12 13:55:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ BoardMember.prototype.veto = function(){
   return `No, I must disagree`;
 }
 BoardMember.prototype.approve = function(){
-  returns `You can do that!`;
+  return `You can do that!`;
 }
 BoardMember.prototype.doCharity = function(){
   return `I like to help people.`;
